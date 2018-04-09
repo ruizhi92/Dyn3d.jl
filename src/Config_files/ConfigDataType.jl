384042e7696f9e6f0a352599a31d224635bf6c47
ruizhi <rzyang92@gmail.com>
2018-04-09 11:09:00 -0700

    Add SingleBody struct.

--- src/Config_files/ConfigDataType.jl
+++ src/Config_files/ConfigDataType.jl
@@ -1,36 +1,54 @@
 module ConfigDataType
 
-export config_body, config_joint
+export ConfigBody, ConfigJoint, Dof, Motions
+
+import Base: show
+
+# motion parameters to describe a motion
+struct Motions
+    motion_type::String
+    motion_params::Vector{Float64}
+end
+
+# single dof information for every dof in a joint
+struct Dof
+    dof_id::Int
+    dof_type::String
+    stiff::Float64
+    damp::Float64
+    qJ_init::Float64
+    motion::Motions
+end
 
 # configuration properties of a single body
-struct config_body
+struct ConfigBody
     nbody::Int
     nverts::Int
     verts::Array{Float64,2}
     ρ::Float64
 end
 
+ConfigBody(nbody) = ConfigBody(nbody,4,
+                                [0. 0.;
+                                 1. 0.;
+                                 1. 1./nbody;
+                                 0. 1./nbody],0.01)
+
+function show(io::IO, m::ConfigBody)
+    println(io, " nbody=$(m.nbody)")
+end
+
 # configuration properties of a single joint
-struct config_joint
+struct ConfigJoint
     joint_type::String
     joint_id::Int
     shape::Tuple{Vector{Float64},Vector{Float64}}
     body1::Int
-    joint_dof::Vector{dof}
+    joint_dof::Vector{Dof}
 end
 
-# single dof information for every dof in a joint
-abstract type dof with
-    dof_id::Int
-    dof_type::String
-    stiff::Float64
-    damp::Float64
-    motion::motion
-end
 
-# motion parameters to describe a motion
-struct motion
 
-end
+
 
 end
--- src/ConstructSystem.jl
+++ src/ConstructSystem.jl
@@ -1,10 +1,11 @@
 module ConstructSystem
 
 # export
-export AddBody
+export SingleBody, SingleJoint, AddBody, AddJoint, AssembleSystem
 
 # use registered packages
 using DocStringExtensions
+import Base: show
 
 # import self-defined modules
 using ..ConfigDataType
@@ -16,8 +17,72 @@ This module construct the body-joint system by:
     3. AssembleSystem
 """
 
+#-------------------------------------------------------------------------------
+mutable struct SingleBody
+    # hierarchy info
+    id::Int
+    pid::Int
+    chid::Vector{Int}
+    nchild::Int
+    # verts
+    nverts::Int
+    verts::Array{Float64,2}
+    verts_i::Array{Float64,2}
+    # coord in body frame
+    x_c::Vector{Float64}
+    x_0::Vector{Float64}
+    # mass and inertia
+    mass::Float64
+    inertia_c::Array{Float64,2}
+    inertia_0::Array{Float64,2}
+    # transform matrix
+    Xb_to_c::Array{Float64,2}
+    Xb_to_i::Array{Float64,2}
+    Xp_to_b::Array{Float64,2}
+    # motion vectors
+    q::Vector{Float64}
+    v::Vector{Float64}
+    c::Vector{Float64}
+    # articulated body info
+    pA::Vector{Float64}
+    Ib_A::Array{Float64,2}
+end
+
+# outer constructor
+SingleBody() = SingleBody(
+    0,0,Vector{Int}(0),0,
+    0,Array{Float64,2}(0,0),Array{Float64,2}(0,0),
+    Vector{Float64}(0),Vector{Float64}(0),
+    0,Array{Float64,2}(0,0),Array{Float64,2}(0,0),
+    Array{Float64,2}(0,0),Array{Float64,2}(0,0),Array{Float64,2}(0,0),
+    Vector{Float64}(0),Vector{Float64}(0),Vector{Float64}(0),
+    Vector{Float64}(0),Array{Float64,2}(0,0)
+)
+
+function show(io::IO, m::SingleBody)
+    println(io, "body_id = $(m.id)", ", parent_id = $(m.pid)",
+            ", nchild = $(m.nchild)", ", child_id = $(m.chid)")
+    println(io, "nverts = $(m.nverts)", ", verts = $(m.verts)")
+    println(io, "verts_i = $(m.verts_i)")
+    println(io, "x_c = $(m.x_c)", ", x_0 = $(m.x_0)")
+    println(io, "mass = $(m.mass)")
+    println(io, "inertia_c = $(m.inertia_c)")
+    println(io, "inertia_0 = $(m.inertia_0)")
+    println(io, "Xb_to_c = $(m.Xb_to_c)")
+    println(io, "Xb_to_i = $(m.Xb_to_i)")
+    println(io, "Xp_to_b = $(m.Xp_to_b)")
+    println(io, "q = $(m.q)", ", v = $(m.v)", ", c = $(m.c)")
+end
+
+#-------------------------------------------------------------------------------
+mutable struct SingleJoint
+
+
+end
+
+#-------------------------------------------------------------------------------
 # mutable struct
-function AddBody(b::config_body)
+function AddBody(b::ConfigBody)
     return 1
 end
 
