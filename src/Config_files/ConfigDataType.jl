83f486bfb839ff9c94c9074df3c2dc9487ad464b
ruizhi <rzyang92@gmail.com>
2018-04-11 10:47:24 -0700

    Finish SingleJoint structure and some of AddBody method

--- src/Config_files/ConfigDataType.jl
+++ src/Config_files/ConfigDataType.jl
@@ -4,22 +4,28 @@ export ConfigBody, ConfigJoint, Dof, Motions
 
 import Base: show
 
+#-------------------------------------------------------------------------------
 # motion parameters to describe a motion
 struct Motions
     motion_type::String
     motion_params::Vector{Float64}
 end
 
+Motions() = Motions("hold", [0])
+#-------------------------------------------------------------------------------
 # single dof information for every dof in a joint
 struct Dof
     dof_id::Int
     dof_type::String
     stiff::Float64
     damp::Float64
-    qJ_init::Float64
+    qJ_init::Vector{Float64}
     motion::Motions
 end
 
+Dof() = Dof(0, "active", 0., 0.,
+    [0., 0., 0., 0., 0., 0.], Motions())
+#-------------------------------------------------------------------------------
 # configuration properties of a single body
 struct ConfigBody
     nbody::Int
@@ -28,26 +34,33 @@ struct ConfigBody
     ρ::Float64
 end
 
-ConfigBody(nbody) = ConfigBody(nbody,4,
-                                [0. 0.;
-                                 1. 0.;
-                                 1. 1./nbody;
-                                 0. 1./nbody],0.01)
+ConfigBody(nbody) = ConfigBody(nbody, 4,
+    [0. 0.; 1. 0.; 1. 1./nbody; 0. 1./nbody], 0.01)
 
 function show(io::IO, m::ConfigBody)
     println(io, " nbody=$(m.nbody)")
 end
 
+#-------------------------------------------------------------------------------
 # configuration properties of a single joint
 struct ConfigJoint
-    joint_type::String
+    njoint::Int
     joint_id::Int
-    shape::Tuple{Vector{Float64},Vector{Float64}}
+    joint_type::String
+    shape1::Vector{Float64}
+    shape2::Vector{Float64}
     body1::Int
     joint_dof::Vector{Dof}
 end
 
+ConfigJoint(njoint,joint_type) = ConfigJoint(njoint, 1, joint_type,
+    [0., 0., 0., 1., 0., 0.],
+    [0., 0., 0., 0., 0., 0.],
+    0, [Dof()])
 
+function show(io::IO, m::ConfigJoint)
+    println(io, " joint_type=$(m.joint_type)")
+end
 
 
 
--- src/ConstructSystem.jl
+++ src/ConstructSystem.jl
@@ -20,7 +20,7 @@ This module construct the body-joint system by:
 #-------------------------------------------------------------------------------
 mutable struct SingleBody
     # hierarchy info
-    id::Int
+    bid::Int
     pid::Int
     chid::Vector{Int}
     nchild::Int
@@ -60,7 +60,7 @@ SingleBody() = SingleBody(
 )
 
 function show(io::IO, m::SingleBody)
-    println(io, "body_id = $(m.id)", ", parent_id = $(m.pid)",
+    println(io, "body_id = $(m.bid)", ", parent_id = $(m.pid)",
             ", nchild = $(m.nchild)", ", child_id = $(m.chid)")
     println(io, "nverts = $(m.nverts)", ", verts = $(m.verts)")
     println(io, "verts_i = $(m.verts_i)")
@@ -76,19 +76,161 @@ end
 
 #-------------------------------------------------------------------------------
 mutable struct SingleJoint
+    # hierarchy info
+    jid::Int
+    joint_type::String
+    body1::Int
+    shape1::Vector{Float64}
+    shape2::Vector{Float64}
+    # dof info
+    nudof::Int
+    ncdof::Int
+    np::Int
+    na::Int
+    udof::Vector{Float64} # newline in constructor
+    cdof::Vector{Float64}
+    udof_p::Vector{Float64} # newline in constructor
+    udof_a::Vector{Float64}
+    i_udof_p::Vector{Float64} # newline in constructor
+    i_udof_a::Vector{Float64}
+    udof_map::Vector{Float64}
+    # dof info modified by HERK
+    nudof_HERK::Int
+    ncdof_HERK::Int
+    udof_HERK::Vector{Float64}
+    cdof_HERK::Vector{Float64}
+    # joint basis matrix
+    S::Array{Float64,2}
+    T::Array{Float64,2}
+    T_HERK::Array{Float64,2}
+    # joint dof
+    joint_dof::Vector{Dof}
+    # motion vectors
+    qJ::Vector{Float64}
+    vJ::Vector{Float64}
+    cJ::Vector{Float64}
+    # transform matrix
+    Xj::Array{Float64,2}
+    Xp_to_j::Array{Float64,2}
+    Xj_to_ch::Array{Float64,2}
+end
 
+# outer constructor
+SingleJoint() = SingleJoint(
+    0," ",0,Vector{Float64}(0),Vector{Float64}(0),
+    0,0,0,0,
+    Vector{Float64}(0),Vector{Float64}(0),
+    Vector{Float64}(0),Vector{Float64}(0),
+    Vector{Float64}(0),Vector{Float64}(0),Vector{Float64}(0),
+    0,0,Vector{Float64}(0),Vector{Float64}(0),
+    Array{Float64,2}(0,0),Array{Float64,2}(0,0),Array{Float64,2}(0,0),
+    [Dof()], # dof
+    Vector{Float64}(0),Vector{Float64}(0),Vector{Float64}(0),
+    Array{Float64,2}(0,0),Array{Float64,2}(0,0),Array{Float64,2}(0,0)
+)
 
+function show(io::IO, m::SingleJoint)
+    println(io, "joint_id = $(m.jid)", ", joint_type = $(m.joint_type)",
+            ", pid = $(m.body1)")
+    println(io, "shape1 = $(m.shape1)", ", shape2 = $(m.shape2)")
+    println(io, "nudof = $(m.nudof)", ", ncdof = $(m.ncdof)",
+            ", np = $(m.np)", ", na = $(m.na)")
+    println(io, "udof = $(m.udof)", ", cdof = $(m.cdof)")
+    println(io, "udof_p = $(m.udof_p)", ", udof_a = $(m.udof_a)")
+    println(io, "i_udof_p = $(m.i_udof_p)", ", i_udof_a = $(m.i_udof_a)")
+    println(io, "udof_map = $(m.udof_map)")
+    println(io, "nudof_HERK = $(m.nudof_HERK)",", ncdof_HERK = $(m.ncdof_HERK)")
+    println(io, "udof_HERK = $(m.udof_HERK)", ", cdof_HERK = $(m.cdof_HERK)")
+    println(io, "S = $(m.S)")
+    println(io, "T = $(m.T)")
+    println(io, "T_HERK = $(m.T_HERK)")
+    println(io, "joint_dof = $(m.joint_dof)")
+    println(io, "Xj = $(m.Xj)")
+    println(io, "Xp_to_j = $(m.Xp_to_j)")
+    println(io, "Xj_to_ch = $(m.Xj_to_ch)")
+    println(io, "qJ = $(m.qJ)", ", vJ = $(m.vJ)", ", cJ = $(m.cJ)")
 end
 
 #-------------------------------------------------------------------------------
-# mutable struct
-function AddBody(b::ConfigBody)
-    return 1
+mutable struct System
+
 end
 
 
-function AddJoint()
+#-------------------------------------------------------------------------------
+# add a single body
+function AddBody(id::Int, cf::ConfigBody, b::SingleBody)
+    # hierarchy info
+    b.bid = id
 
+    # verts
+    b.nverts = cf.nverts
+    b.verts = zeros(Float64, b.nverts, 3)
+    b.verts[:,1] = cf.verts[:,2]
+    b.verts[:,3] = cf.verts[:,1]
+    b.verts_i = b.verts
+
+    # x_c
+    Xc = 0.; Zc = 0.; A = 0.; Ix = 0.; Iz = 0.; Ixz = 0.
+
+    for i = 1:b.nverts
+        xj = b.verts[i,1]
+        zj = b.verts[i,3]
+        if i == b.nverts
+            xjp1 = b.verts[1,1]
+            zjp1 = b.verts[1,3]
+        else
+            xjp1 = b.verts[i+1,1]
+            zjp1 = b.verts[i+1,3]
+        end
+        fact = zj*xjp1 - zjp1*xj
+        Xc = Xc + (xj + xjp1)*fact
+        Zc = Zc + (zj + zjp1)*fact
+        A  = A  + 0.5*fact
+    end
+
+    Xc = Xc/(6.*A); Zc = Zc/(6.*A)
+    b.x_c = [Xc, 0., Zc]
+
+    # mass, inertia_c
+    b.mass  = cf.ρ*A
+    for i = 1:b.nverts
+        # make (Zc,Xc) the origin
+        xj = b.verts[i,1] - Xc
+        zj = b.verts[i,3] - Zc
+        if i == b.nverts
+            xjp1 = b.verts[1,1] - Xc
+            zjp1 = b.verts[1,3] - Zc
+        else
+            xjp1 = b.verts[i+1,1] - Xc
+            zjp1 = b.verts[i+1,3] - Zc
+        end
+        fact = zj*xjp1 - zjp1*xj
+        Ix =   Ix + (zj^2+zj*zjp1+zjp1^2)*fact
+        Iz =   Iz + (xj^2+xj*xjp1+xjp1^2)*fact
+        Ixz = Ixz + (xj*zjp1+2.*xj*zj+2.*xjp1*zjp1+xjp1*zj)*fact
+    end
+    Ix = Ix/12.
+    Iz = Iz/12.
+    Iy = Ix + Iz
+    Ixz = Ixz/24.
+
+    # inertia in 3d form, transform to 6d
+    inertia_3d = cf.ρ*[Ix 0. -Ixz; 0. Iy 0.; -Ixz 0. Iz]
+    mass_3d = b.mass*eye(3)
+    b.inertia_c = [inertia_3d zeros(Float64,3,3);
+                   zeros(Float64,3,3) mass_3d]
+
+    # Xb_to_c
+    # wait for trans_matrix
+
+    return b
+end
+
+#-------------------------------------------------------------------------------
+# add a single joint
+function AddJoint(cf::ConfigJoint, j::SingleJoint)
+    return j
 end
 
 function AssembleSystem()
