d37866ae8168c89a5fcc753568a1f7a7729f1d4e
autodocs <autodocs>
2018-06-18 22:32:41 +0000

    build based on eb6868b

---
+++ latest/search_index.js
@@ -0,0 +1,43 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Dyn3d-1",
+    "page": "Home",
+    "title": "Dyn3d",
+    "category": "section",
+    "text": "A 2d/3d rigid body dynamics solver"
+},
+
+{
+    "location": "index.html#Installation-1",
+    "page": "Home",
+    "title": "Installation",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "manual/system.html#",
+    "page": "System",
+    "title": "System",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "manual/system.html#System-1",
+    "page": "System",
+    "title": "System",
+    "category": "section",
+    "text": ""
+},
+
+]}
