2813f8465d1aa68b17e4a38a16c436001d9e4cad
autodocs <autodocs>
2018-07-11 00:06:34 +0000

    build based on 1685e60

--- latest/search_index.js
+++ latest/search_index.js
@@ -13,31 +13,47 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Dyn3d",
     "category": "section",
-    "text": "A 2d/3d rigid body dynamics solver"
+    "text": "A 2d/3d rigid body dynamics solverThe main goal of this repository is to construct a rigid body-joint system and solve forward/backward dynamics problem on it. This package is functioned throughconstructing 2d/3d rigid body-joints system\nsolving motions on unconstrained degrees of freedom(passive joints) andforces on constrained degrees of freedom(active joints).plotting in Julia and making movies in MatlabTo solve a rigid body dynamics problem, this package express the dynamics using 6D spatial vector developed by Roy Featherstone. The governing equations are formulated to fit in half explicit Runge-Kutta method on index-2 differential equations. Constrained forces on joints are represented in Lagrange multiplier terms and solved together with motions of all degrees of freedom."
 },
 
 {
-    "location": "index.html#Installation-1",
-    "page": "Home",
-    "title": "Installation",
-    "category": "section",
-    "text": ""
-},
-
-{
-    "location": "manual/system.html#",
-    "page": "System",
-    "title": "System",
+    "location": "manual/construct_system.html#",
+    "page": "Construct system",
+    "title": "Construct system",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "manual/system.html#System-1",
-    "page": "System",
-    "title": "System",
+    "location": "manual/construct_system.html#Construct-system-1",
+    "page": "Construct system",
+    "title": "Construct system",
     "category": "section",
     "text": ""
+},
+
+{
+    "location": "manual/construct_system.html#Build-in-Types-1",
+    "page": "Construct system",
+    "title": "Build-in Types",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "manual/construct_system.html#Methods-1",
+    "page": "Construct system",
+    "title": "Methods",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "manual/construct_system.html#Index-1",
+    "page": "Construct system",
+    "title": "Index",
+    "category": "section",
+    "text": "Pages = [\"construct_system.md\"]"
 },
 
 ]}
