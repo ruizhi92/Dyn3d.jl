94e1f49861a1fa5ad41b7a73ab2c8539e5c6aab7
ruizhi <rzyang92@gmail.com>
2018-11-13 12:19:20 -0800

    Exclude Whirl from unit test

--- test/FluidInteraction.jl
+++ test/FluidInteraction.jl
@@ -15,10 +15,8 @@
     herkbody = Dyn3d.HERKBody(system.num_params,HERKFuncM, HERKFuncGT, HERKFuncG,
                 (HERKFuncf,HERKFuncgti), (UpdatePosition!,UpdateVelocity!))
 
-    # Construct fluid
-    include(Pkg.dir("Dyn3d")*"/test/config_fluid.jl")
-
     # create fluid-body interface
+    Δx = 0.02
     bgs = GenerateBodyGrid(bd; np=DetermineNP(nbody, Δx))
     @test bgs[1].np == 201
     @test length(bgs[1].points) == 201
@@ -52,31 +50,27 @@
     @test all(bkins[3][:,2] .≈ 0.99995)
     @test all(bkins[3][:,4] .≈ -0.01)
 
-    # create fluid solver and advance for one step
-    coord_init = bkins[1][:,1:2]
-    X̃ = VectorData(coord_init)
-    sys = Systems.NavierStokes((nx,ny),Re,Δx,Δt,U∞ = U∞, X̃ = X̃, isstore = true, isstatic = false)
-    u = w₀
-    f = VectorData(X̃)
-    fs = [[f.u f.v] for i=1:NS]
-    ifherk_sc2d = Whirl.IFHERK_sc2d(u,f,sys.Δt,
-                    (t,u) -> Systems.plan_intfact(t,u,sys),
-                    (u,t,coord) -> Whirl.plan_constraints(u,t,sys,coord),
-                    ((u,t) -> Whirl.r₁(u,t,sys),
-                     (u,t,motion) -> Whirl.r₂(u,t,sys,motion)),
-                    coord_init,
-                    tol=1e-3,rk=Whirl.TimeMarching.RK31,
-                    isstored=true,isstaticconstraints=false)
-    t, u, f, fs = ifherk_sc2d(t,u,bkins)
+    # integrate body force and test
+    f = [-0.649779 -0.785108; 0.313551 -0.834843; -0.328143 -1.0316; 0.271288 -1.14996;
+     -0.280372 -1.26448; 0.250311 -1.36147; -0.249826 -1.44837; 0.227311 -1.52607;
+      -0.222081 -1.59596; 0.203026 -1.6592; -0.195263 -1.71636; 0.178144 -1.76822;
+       -0.168865 -1.81507; 0.152962 -1.85744; -0.142688 -1.89551; 0.127615 -1.92966;
+        -0.116636 -1.95998; 0.102171 -1.98676; -0.0906599 -2.01005; 0.0766686 -2.03005;
+         -0.0647301 -2.04679; 0.0511295 -2.06041; -0.0388282 -2.07091; 0.0255696 -2.0784;
+          -0.0129412 -2.08286; 2.84082e-14 -2.08437; 0.0129412 -2.08286; -0.0255696 -2.0784;
+           0.0388282 -2.07091; -0.0511295 -2.06041; 0.0647301 -2.04679; -0.0766686 -2.03005;
+            0.0906599 -2.01005; -0.102171 -1.98676; 0.116636 -1.95998; -0.127615 -1.92966;
+             0.142688 -1.89551; -0.152962 -1.85744; 0.168865 -1.81507; -0.178144 -1.76822;
+              0.195263 -1.71636; -0.203026 -1.6592; 0.222081 -1.59596; -0.227311 -1.52607;
+               0.249826 -1.44837; -0.250311 -1.36147; 0.280372 -1.26448; -0.271288 -1.14996;
+                0.328143 -1.0316; -0.313551 -0.834843; 0.649779 -0.785108]
 
-    # integrate body force
-    for i = 1:size(fs[1],1)
-        bgs[1].f_ex3d[i][[1,2]] = fs[3][i,:]*Δx^2
+    for i = 1:size(f,1)
+        bgs[1].f_ex3d[i][[1,2]] = f[i,:]*Δx^2
     end
     bgs = IntegrateBodyGridDynamics(bd,bgs)
 
-    # there's no rotation for this special case
-    @test isapprox(sum(fs[3][:,1])*Δx^2, bgs[1].f_ex6d[4]; atol=1e-7)
-    @test isapprox(sum(fs[3][:,2])*Δx^2, bgs[1].f_ex6d[5]; atol=1e-7)
+    @test isapprox(sum(f[:,1])*Δx^2, bgs[1].f_ex6d[4]; atol=1e-7)
+    @test isapprox(sum(f[:,2])*Δx^2, bgs[1].f_ex6d[5]; atol=1e-7)
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,6 @@ using TestSetExtensions
 
 include(Pkg.dir("Dyn3d")*"/src/Dyn3d.jl")
 using Dyn3d
-include(Pkg.dir("Whirl")*"/src/Whirl.jl")
-using Whirl
 
 @test isempty(detect_ambiguities(Dyn3d))
 
